07dcb1c08b6826c855f87f625d4f66ecc99ecdbc
Caio Fernandes <caio_fsouza@hotmail.com>
2017-07-20 19:05:43 -0300

    More studies

--- DOMSlim.js
+++ DOMSlim.js
@@ -49,6 +49,50 @@
       } else {
         return creator(elem, attributes)
       }
+    },
+    stringToHtml (text) {
+      const notHasClosedTag = [
+        'input',
+        'br',
+        'hr'
+      ]
+      const regexTag = /(\<)(\w+)/
+      let elementTree
+
+      const getNextElementTag = (_text) => {
+        let elementTag = _text.match(regexTag)[2]
+        return elementTag
+      }
+
+      const createElementTree = (_elementTree, _text, parentNode) => {
+        let newElementTag = getNextElementTag(_text)
+        let removeCreatedElement = new RegExp(`(\\<${newElementTag}\\>)([\\s\\W\\w\\d\\D]+)(\\<\\/${newElementTag}\\>)`)
+        let restOfText = _text.replace(removeCreatedElement, '$2')
+        console.log(restOfText, removeCreatedElement)
+        let newElement = this.createEl(newElementTag)
+
+        if (parentNode === null) {
+          parentNode = newElement
+          _elementTree = newElement
+        } else {
+          parentNode.appendChild(newElement)
+        }
+
+
+        if (regexTag.test(restOfText)) {
+          parentNode = newElement
+          let matches = restOfText.match(/(\<)(\w+)/g)
+          let matchesFormatted = matches.map(m => m.replace(/\</, ''))
+
+          console.log(matchesFormatted)
+          // createElementTree(_elementTree, restOfText)
+        } else {
+          return _elementTree
+        }
+      }
+
+      return createElementTree(elementTree, text, null)
+
     }
   }
 
